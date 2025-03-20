17bcc678edf1b85d84ca8d682c1422b4fd0eb6d1
Giacomo Salsano <154028716+giacomosalsano@users.noreply.github.com>
2025-03-20 18:01:33 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
